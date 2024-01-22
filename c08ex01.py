fd0e8ddafb795db3ca601fc8c84cd98ab34e5597
Carlos Augusto de Sousa <41213544+casc12@users.noreply.github.com>
2024-01-22 16:38:32 -0300

    Update c08ex01.py

--- c08ex01.py
+++ c08ex01.py
@@ -3,12 +3,12 @@
         b = []\n
         
         
-        print(\"Exemplo de checagem de índice\\n \")
+        print("Exemplo de checagem de índice\n")
         
         #Entrada de dados\n
         
         for i in range(10):
-          valor = int(input(\"Entre como {0:2}º valor: \".format(i+1)))
+          valor = int(input("Entre como {0:2}º valor: \".format(i+1)))
           a.append(valor)
            #print(a[i])
         
@@ -18,18 +18,18 @@
         for i in range(10):
           if( i % 2 == 0):
             b.append(a[i] * 5)\n",
-          else:\n
+          else:
             b.append(a[i] + 5)
         
         
         #Apresentações da listas
         print()
         for i in range(10):
-          print(\"A[{0:2}] = {1:4} na posição {2:2}.\".format(i + 1, a[i], i ))
+          print("A[{0:2}] = {1:4} na posição {2:2}.".format(i + 1, a[i], i ))
         
-        print()\n
+        print()
         for i in range(10):\n
-          print(\"B[{0:2}] = {1:4} na  posição {2:2}\".format(i + 1, b[i], i))
+          print("B[{0:2}] = {1:4} na  posição {2:2}\".format(i + 1, b[i], i))
         
         enter = input("\nPressione <Enter> para encerrar ...")
       
