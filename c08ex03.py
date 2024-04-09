b132f2ca2a8b082dedae846fe66a897b2d33c8ef
Carlos Augusto de Sousa <41213544+casc12@users.noreply.github.com>
2024-04-09 14:39:14 -0300

    Criado usando o Colaboratory

---
+++ c08ex03.py
@@ -0,0 +1,72 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNiPnI4bXLdwZ+NdhyAaEof",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/casc12/Introdu-o_a_liguagem_Python/blob/Cap08/c08ex03.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [],
+      "metadata": {
+        "id": "6v0v1MlQ71uE"
+      }
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "a = []\n",
+        "s = 0\n",
+        "\n",
+        "print(\"Somatório de valores informados\")"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "zOEJa7xh72Gn",
+        "outputId": "b1076fa2-3b0b-430f-def9-f1867a273553"
+      },
+      "execution_count": 10,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Somatório de valores informados\n"
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "8Vosg56GSBnu"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
