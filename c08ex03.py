3aa3eb6014e83c816e4de1c38ee4fc2b469b6758
Carlos Augusto de Sousa <41213544+casc12@users.noreply.github.com>
2024-04-09 15:10:35 -0300

    Update c08ex03.py

--- c08ex03.py
+++ c08ex03.py
@@ -1,13 +1,13 @@
-a = []\n
-s = 0\n
+a = []
+s = 0
         
 print("Somatório de valores informados")
        
 #Entrada de Dados
-i = 0\n
+i = 0
 resp = 'S'
 while(resp.upper() == 'S'):
-  a.append(float(input(\"\\nEntre o {0:3})o. valor: \".format(i+1))))
+  a.append(float(input(\"\nEntre o {0:3})o. valor: ".format(i+1))))
   s += a[i]
   i += 1
   print()
@@ -16,10 +16,10 @@ while(resp.upper() == 'S'):
 # Apresentação dos Dados
 print()
 for i in range(len(a)):
-  print(\"A[{0:3}] = {1:8,.2f} na posição {2:3}.\".format(i + 1,a[i],i))
+  print("A[{0:3}] = {1:8,.2f} na posição {2:3}.\".format(i + 1,a[i],i))
   
 print()
-print(\"Soma dos valores informados= {0:8,.2f}\".format(s))
+print("Soma dos valores informados= {0:8,.2f}\".format(s))
         
 enter = input("\nPressione <Enter> para encerrar... ")
    
