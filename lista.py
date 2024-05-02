88d46ea4499e5a0d743df5cb283e2e32525e561b
Carlos Augusto de Sousa <41213544+casc12@users.noreply.github.com>
2024-05-02 14:47:41 -0300

    Update lista.py

--- lista.py
+++ lista.py
@@ -70,114 +70,110 @@ listay = listax * 3
 print(listay)
        
 #trabalahando com a lista 10, fatiamento\n",
-lista10 = [1,2,3,4,5,6,7,8,9,0]\n",
-print(lista10[2:5])\n",
-        "print(lista10[:5])\n",
-        "print(lista10[2:])\n",
-        "# fatiamento com número negativo\n",
-        "print(lista10[-5:-3])\n",
-        "print(lista10[:-3])\n",
-        "print(lista10[-5:])\n",
-        "print(lista10[-5:3])\n",
-        "\n",
-        "# trabalhando com o Lista11 , usando a concatenação como alternativa ao append\n",
-        "\n",
-        "lista11 = [1,2,3,4,5]\n",
-        "print(lista11)\n",
-        "lista11 = lista11 +[\"a\"]\n",
-        "print(lista11)\n",
-        "lista11 += [\"b\"]\n",
-        "print(lista11)\n",
-        "lista11 += [6,7,8,9,0]\n",
-        "print(lista11)\n",
-        "\n",
-        "# Trabalhando com o lista12, usando o metodo extend\n",
-        "\n",
-        "lista12 = []\n",
-        "print(lista12)\n",
-        "lista12.append(9)\n",
-        "print(lista12)\n",
-        "lista12.append(8)\n",
-        "print(lista12)\n",
-        "\n",
-        "lista12.extend([7])\n",
-        "print(lista12)\n",
-        "lista12.extend([6,5,4])\n",
-        "print(lista12)\n",
-        "lista12.append([3,2,1])\n",
-        "print(lista12)\n",
-        "print(len(lista12))\n",
-        "\n",
-        "# Trabalhando com lista13, usando o short para ordenar uma list\n",
-        "lista13 = [\"Silvio\",\"Maria\",\"Silvia\",\"Mario\",\"Augusto\"]\n",
-        "lista13.sort()\n",
-        "print(lista13)\n",
-        "\n",
-        "#trabalhando com lista14, usando o short para autera a ordem de exibição usando o reverse\n",
-        "lista14 = [\"Silvio\",\"Maria\",\"Silvia\",\"Mario\",\"Augusto\"]\n",
-        "lista14.sort(reverse= True)\n",
-        "print(lista14)\n",
-        "\n",
-        "#trabalhando com lista15, usando o sorted() para auterar a ordem de exibição usando reverse\n",
-        "lista15 = [8,2,6,4,0,9,1,3,7,5]\n",
-        "# print(lista15)\n",
-        "# print(\" 12 \")\n",
-        "\n",
-        "#organizando a lista\n",
-        "x = sorted(lista15)\n",
-        "print(x)\n",
-        "print(\"\\nComando\\n\")\n",
-        "# ordem inversa numerica\n",
-        "sorted(lista15, reverse = True)\n",
-        "\n",
-        "#acessadno elementos da lista por index()\n",
-        "lista16 = [8,2,6,4,0,9,1,3,7,5]\n",
-        "print(lista16.index(9))\n",
-        "#print(lista16.index(10))\n",
-        "\n",
-        "# usando o comando del para remover um intem de lista\n",
-        "lista17 = [11,22,33,44,66,77,88,99]\n",
-        "print(lista17)\n",
-        "del lista17[3]\n",
-        "\n",
-        "print(lista17)\n",
-        "\n",
-        "del lista17[4:6]\n",
-        "\n",
-        "print(lista17)\n",
-        "# voce poderia ultilizar del lista17[:] mas vou cololcar o Lista17.clear()\n",
-        "lista17.clear()\n",
-        "print(lista17)\n",
-        "\n",
-        "# ultilizando pop() com a lista18\n",
-        "lista18 = [2,4,6,8,10,12,14,16,18,20]\n",
-        "print(lista18)\n",
-        "v1 = lista18.pop()\n",
-        "print(lista18)\n",
-        "print(v1)\n",
-        "v2 = lista18.pop(4)\n",
-        "print(lista18)\n",
-        "print(v2)\n",
-        "\n",
-        "# ultilizando remove() com a lista19\n",
-        "lista19 = [\"a\",\"b\",\"c\",\"d\",\"e\",\"f\",\"g\"]\n",
-        "print(lista19)\n",
-        "lista19.remove(\"d\")\n",
-        "print(lista19)\n",
-        "\n",
-        "#Criando uma lista partir de um exitente usando a biblioteca Math\n",
-        "lista20 = [1,2,3,4,5]\n",
-        "lista21 = [math.factorial(i) for i in lista20]\n",
-        "\n",
-        "print(lista21)\n",
-        "\n",
-        "# Usando um o métod index através de um elemento dentro de um lista\n",
-        "\n",
-        "lista22 = [1,2,3,\"quatro\",\"cinco\",\"seis\"]\n",
-        "print(lista22)\n",
-        "#vai retornar em qual poisição se encontra o elemento procurado\n",
-        "print(lista22.index(\"quatro\"))\n"
-      ]
-    }
-  ]
-}
+lista10 = [1,2,3,4,5,6,7,8,9,0]
+print(lista10[2:5])
+print(lista10[:5])
+print(lista10[2:])
+# fatiamento com número negativo
+print(lista10[-5:-3])
+print(lista10[:-3])
+print(lista10[-5:])
+print(lista10[-5:3])
+        
+# trabalhando com o Lista11 , usando a concatenação como alternativa ao append
+        
+lista11 = [1,2,3,4,5]
+print(lista11)
+lista11 = lista11 +["a"]
+print(lista11)
+lista11 += ["b"]
+print(lista11)
+lista11 += [6,7,8,9,0]
+print(lista11)
+        
+# Trabalhando com o lista12, usando o metodo extend
+        
+lista12 = []
+print(lista12)
+lista12.append(9)
+print(lista12)
+lista12.append(8)
+print(lista12)
+       
+lista12.extend([7])
+print(lista12)
+lista12.extend([6,5,4])
+print(lista12)
+lista12.append([3,2,1])
+print(lista12)
+print(len(lista12))
+        
+# Trabalhando com lista13, usando o short para ordenar uma list
+lista13 = ["Silvio","Maria","Silvia","Mario","Augusto"]
+lista13.sort()
+print(lista13)
+        
+#trabalhando com lista14, usando o short para autera a ordem de exibição usando o reverse\
+lista14 = ["Silvio","Maria","Silvia","Mario","Augusto"]
+lista14.sort(reverse= True)
+print(lista14)
+        
+#trabalhando com lista15, usando o sorted() para auterar a ordem de exibição usando reverse
+lista15 = [8,2,6,4,0,9,1,3,7,5]
+# print(lista15)
+# print(\" 12 \")
+        
+#organizando a lista
+x = sorted(lista15)
+print(x)
+print("\nComando\n")
+# ordem inversa numerica\n",
+sorted(lista15, reverse = True)\n",
+        
+#acessadno elementos da lista por index()\n",
+lista16 = [8,2,6,4,0,9,1,3,7,5]
+print(lista16.index(9))
+#print(lista16.index(10))
+        
+# usando o comando del para remover um intem de lista
+lista17 = [11,22,33,44,66,77,88,99]
+print(lista17)
+del lista17[3]
+        
+print(lista17)
+        
+del lista17[4:6]
+   
+print(lista17)
+# voce poderia ultilizar del lista17[:] mas vou cololcar o Lista17.clear()
+lista17.clear()
+print(lista17)
+        
+# ultilizando pop() com a lista18
+lista18 = [2,4,6,8,10,12,14,16,18,20]
+print(lista18)
+v1 = lista18.pop()
+print(lista18)\n",
+print(v1)
+v2 = lista18.pop(4)
+print(lista18)
+print(v2)
+        
+# ultilizando remove() com a lista19
+lista19 = ["a","b","c","d","e","f","g"]
+print(lista19)
+lista19.remove("d")
+print(lista19)
+        
+#Criando uma lista partir de um exitente usando a biblioteca Math
+lista20 = [1,2,3,4,5]
+lista21 = [math.factorial(i) for i in lista20]\
+        
+print(lista21)
+        
+# Usando um o métod index através de um elemento dentro de um lista
+        
+lista22 = [1,2,3,"quatro","cinco","seis"]
+print(lista22)
+#vai retornar em qual poisição se encontra o elemento procurado
+print(lista22.index("quatro"))
