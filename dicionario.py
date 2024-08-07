f41fd91d4db0944b6affa60a862bb15df2053aad
Carlos Augusto de Sousa <41213544+casc12@users.noreply.github.com>
2024-08-07 15:42:56 -0300

    Criado usando o Colab

--- dicionario.py
+++ dicionario.py
@@ -1,11 +1,75 @@
-dicionario1 = {"nome":"carlos", "idade":34, "escolaridade":"superior"}
-print(dicionario1["nome"])
-print(dicionario1["idade"])
-print(dicionario1["escolaridade"])
-print(type(dicionario1))
-print(dicionario1)
-dicionario1["idade"]= 25
-print(dicionario1)
-#print(dicionario1["altura"])
-print(dicionario1.get("nome"))
-print(dicionario1.get("altura"))
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMbVXRZ8Yvo1Fz5u+EqkxE2",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/casc12/Introdu-o_a_liguagem_Python/blob/Cap08/dicionario.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "id": "u3HO42xrDssk",
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "outputId": "4393cd6b-2272-475f-d09c-ddbfa4bccd83"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "carlos\n",
+            "34\n",
+            "superior\n",
+            "<class 'dict'>\n",
+            "{'nome': 'carlos', 'idade': 34, 'escolaridade': 'superior'}\n",
+            "{'nome': 'carlos', 'idade': 25, 'escolaridade': 'superior'}\n",
+            "carlos\n",
+            "None\n",
+            "3\n",
+            "<class 'dict'>\n"
+          ]
+        }
+      ],
+      "source": [
+        "dicionario1 = {\"nome\":\"carlos\", \"idade\":34, \"escolaridade\":\"superior\"}\n",
+        "print(dicionario1[\"nome\"])\n",
+        "print(dicionario1[\"idade\"])\n",
+        "print(dicionario1[\"escolaridade\"])\n",
+        "print(type(dicionario1))\n",
+        "print(dicionario1)\n",
+        "dicionario1[\"idade\"]= 25\n",
+        "print(dicionario1)\n",
+        "#print(dicionario1[\"altura\"])\n",
+        "print(dicionario1.get(\"nome\"))\n",
+        "print(dicionario1.get(\"altura\"))\n",
+        "print(len(dicionario1))\n",
+        "print(type(dicionario1))\n",
+        "\n"
+      ]
+    }
+  ]
+}
